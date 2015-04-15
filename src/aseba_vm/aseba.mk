1754f679550f3990bf7542297dc3f582f774f229
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-04-15 16:18:53 +0200

    Correct make rules

--- src/aseba_vm/aseba.mk
+++ src/aseba_vm/aseba.mk
@@ -1,10 +1,9 @@
 ASEBA = ./aseba
 
 ASEBASRC = $(ASEBA)/transport/buffer/vm-buffer.c \
-		   $(ASEBA)/vm/natives.c \
-		   $(ASEBA)/vm/vm.c \
-		   src/aseba_vm/aseba_node.c \
-		   src/aseba_vm/skel.c \
-		   src/aseba_vm/skel_user.c
+           $(ASEBA)/vm/natives.c \
+           $(ASEBA)/vm/vm.c \
+           src/aseba_vm/aseba_node.c \
+           src/aseba_vm/skel.c
 
 ASEBAINC = $(ASEBA)
