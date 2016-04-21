4f56b3dd6d96036b19962139663152765d9b0eee
Antoine Albertelli <antoine@antoinealb.net>
2016-04-21 11:07:25 +0200

    Delete unused submodules

--- src/src.mk
+++ src/src.mk
@@ -2,7 +2,3 @@ SRC += src/panic.c
 SRC += src/cmd.c
 SRC += src/main.c
 SRC += src/usbcfg.c
-SRC += src/cmp/cmp.c
-SRC += src/cmp_mem_access/cmp_mem_access.c
-SRC += src/crc/crc32.c
-SRC += src/serial-datagram/serial_datagram.c
