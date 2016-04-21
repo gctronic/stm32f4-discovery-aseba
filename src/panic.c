1992c58d63eab4b7c3adb1f5938bf597c719ecac
Antoine Albertelli <antoine@antoinealb.net>
2016-04-21 19:12:57 +0200

    Cleanup panic handler

--- src/panic.c
+++ src/panic.c
@@ -5,31 +5,11 @@ void panic_handler(const char *reason)
 {
     (void)reason;
 
-    //Turns on all 4 LEDs
-    palSetPad(GPIOD, 12);
-    palSetPad(GPIOD, 13);
-    palSetPad(GPIOD, 14);
-    palSetPad(GPIOD, 15);
+    palSetPad(GPIOD, GPIOD_LED3);
+    palSetPad(GPIOD, GPIOD_LED4);
+    palSetPad(GPIOD, GPIOD_LED5);
+    palSetPad(GPIOD, GPIOD_LED6);
+    while (true) {
 
-    // static volatile uint32_t ipsr;
-    // static volatile const char *msg;
-    // msg = reason;
-    // ipsr = __get_IPSR();
-
-    // (void)msg;
-    // (void)ipsr;
-
-    // strncpy(panic_buffer, reason, sizeof(panic_buffer));
-
-    // panic_buffer[sizeof(panic_buffer) - 1] = '\0';
-    // panic_buffer_crc = crc32(PANIC_CRC_INIT, panic_buffer, sizeof(panic_buffer));
-
-// #ifdef DEBUG // debug builds block to be able to connect a debugger
-//     while (1) {
-//         // todo print error over uart
-//     }
-
-// #else // non-debug builds reboot in safemode
-//     reboot_in_safemode();
-// #endif
+    }
 }
