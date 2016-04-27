d6e2d81acbd5f3ce9b0e4f069f3eee19438d7ed7
Antoine Albertelli <antoine@antoinealb.net>
2016-04-27 23:31:24 +0200

    Move aseba port related function into aseba node

--- src/aseba_vm/aseba_node.c
+++ src/aseba_vm/aseba_node.c
@@ -12,6 +12,8 @@
 #include "transport/buffer/vm-buffer.h"
 #include "aseba_vm/skel_user.h"
 #include "aseba_vm/aseba_node.h"
+#include "aseba_vm/aseba_bridge.h"
+#include "flash/flash.h"
 
 #include "discovery_demo/accelerometer.h"
 #include "discovery_demo/leds.h"
@@ -152,6 +154,57 @@ void AsebaResetIntoBootloader(AsebaVMState *vm)
     (void) vm;
     NVIC_SystemReset();
 }
+
+void AsebaNativeFunction(AsebaVMState *vm, uint16 id)
+{
+    if (id < nativeFunctions_length) {
+        nativeFunctions[id](vm);
+    } else {
+        AsebaVMEmitNodeSpecificError(vm, "Invalid native function.");
+    }
+}
+
+const AsebaNativeFunctionDescription * const * AsebaGetNativeFunctionsDescriptions(AsebaVMState *vm)
+{
+    (void) vm;
+    return nativeFunctionsDescription;
+}
+
+
+const AsebaVMDescription* AsebaGetVMDescription(AsebaVMState *vm)
+{
+    (void) vm;
+    return &vmDescription;
+}
+
+const AsebaLocalEventDescription * AsebaGetLocalEventsDescriptions(AsebaVMState *vm)
+{
+    (void) vm;
+    return localEvents;
+}
+
+
+uint16 AsebaShouldDropPacket(uint16 source, const uint8* data)
+{
+    /* Accept all packets in bridge mode. */
+    if (aseba_is_bridge()) {
+        return 0;
+    }
+
+    return AsebaVMShouldDropPacket(&vmState, source, data);
+}
+
+void AsebaWriteBytecode(AsebaVMState *vm)
+{
+    extern uint8_t _aseba_bytecode_start;
+
+    flash_unlock();
+    flash_sector_erase(&_aseba_bytecode_start);
+    flash_write(&_aseba_bytecode_start, &vm->bytecodeSize, sizeof(uint16));
+    flash_write(&_aseba_bytecode_start + sizeof(uint16), vm->bytecode, vm->bytecodeSize);
+    flash_lock();
+}
+
 
 
 // This function must update the accelerometer variables
