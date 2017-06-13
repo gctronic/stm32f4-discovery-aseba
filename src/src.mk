17a791d3da79777d1fcf959dee6cd8db0416e1cd
GCtronic <stefano@gctronic.com>
2017-06-13 13:14:37 +0200

    Build errors corrected

---
+++ src/src.mk
@@ -0,0 +1,24 @@
+
+INCDIR += src/
+
+
+CSRC += ./src/cmd.c
+CSRC += ./src/discovery_demo/accelerometer.c
+CSRC += ./src/discovery_demo/button.c
+CSRC += ./src/discovery_demo/leds.c
+CSRC += ./src/flash/flash.c
+CSRC += ./src/main.c
+CSRC += ./src/memory_protection.c
+CSRC += ./src/panic.c
+CSRC += ./src/usbcfg.c
+CSRC += src/chibios-syscalls/malloc_lock.c
+CSRC += src/chibios-syscalls/newlib_syscalls.c
+CSRC += ./src/config_flash_storage.c
+CSRC += src/cmp/cmp.c
+CSRC += src/cmp_mem_access/cmp_mem_access.c
+CSRC += src/crc/crc16.c
+CSRC += src/crc/crc32.c
+CSRC += src/parameter/parameter.c
+CSRC += src/parameter/parameter_msgpack.c
+CSRC += src/parameter/parameter_print.c
+CSRC += src/camera/po8030.c
